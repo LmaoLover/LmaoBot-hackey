766e1a7fa10eb8258ba4d45e90f45ac5b0736ac8
LmaoLover <hello@lmao.love>
2025-08-16 18:27:32 -0600

    Login access token

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def main():
-    print("Hello from lmaobot-hackey!")
+import os
+import sys
+import requests
+from dotenv import load_dotenv
+
+
+def login_access_token(login_url, username, password):
+    headers = {
+        "x-csrf-token": "1",
+        "Content-Type": "application/json",
+    }
+
+    payload = {
+        "user": username,
+        "pass": password,
+    }
+
+    response = requests.post(login_url, headers=headers, json=payload)
+    response.raise_for_status()
+
+    data = response.json()
+    token = data["data"]["sessionToken"]
+
+    return token
 
 
 if __name__ == "__main__":
-    main()
+    load_dotenv()
+
+    username = os.environ.get("HACKEY_USER")
+    password = os.environ.get("HACKEY_PASSWORD")
+    login_url = os.environ.get("HACKEY_LOGIN_URL")
+    websocket_url = os.environ.get("HACKEY_WEBSOCKET_URL")
+
+    try:
+        access_token = login_access_token(login_url, username, password)
+    except:
+        print("Could not login")
+        sys.exit(1)
+
+    print(access_token)
